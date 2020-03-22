2afe8d05060004ca6c06f480af7672bf193d8280
Muchromi <muchromiahmad@gmail.com>
2020-03-22 21:58:41 +0700

    berhasil lagi

---
+++ matrial.php
@@ -0,0 +1,101 @@
+<?php
+    include_once('header.php');
+?>
+<div class="row">
+    <div class="column full">
+        <p>Data of all Matrials NSC Learning</p>
+        <div class="row">
+            <div class="col-25">
+                <input type="search" name="search" placeholder="Search....">
+            </div>
+        </div><br>
+        <div style="overflow-x:auto;">
+            <table>
+                <tr>
+                    <th>No.</th>
+                    <th>Matrial Name</th>
+                    <th>Description</th>
+                    <th>Date Post</th>
+                    <th>Action</th>
+                </tr>
+
+                <tr>
+                    <td>1</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>Sas</td>
+                    <td>
+                        <a class="btn btn-default" href=""><i class="fa fa-download" aria-hidden="true"></i> Download</a>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>2</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>Sas</td>
+                    <td>
+                        <a class="btn btn-default" href=""><i class="fa fa-download" aria-hidden="true"></i> Download</a>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>3</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>Sas</td>
+                    <td>
+                        <a class="btn btn-default" href=""><i class="fa fa-download" aria-hidden="true"></i> Download</a>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>4</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>Sas</td>
+                    <td>
+                        <a class="btn btn-default" href=""><i class="fa fa-download" aria-hidden="true"></i> Download</a>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>5</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>Sas</td>
+                    <td>
+                        <a class="btn btn-default" href=""><i class="fa fa-download" aria-hidden="true"></i> Download</a>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>6</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>Sas</td>
+                    <td>
+                        <a class="btn btn-default" href=""><i class="fa fa-download" aria-hidden="true"></i> Download</a>
+                    </td>
+                </tr>
+
+             </table>
+
+        </div>
+
+        <div class="pagination">
+            <a href="#">&laquo;</a>
+            <a href="#">1</a>
+            <a class="active" href="#">2</a>
+            <a href="#">3</a>
+            <a href="#">4</a>            
+            <a href="#">5</a>
+            <a href="#">6</a> 
+            <a href="#">&raquo;</a> 
+        </div>
+    </div>
+</div>
+
+<?php
+    include_once('footer.php');
+?>
